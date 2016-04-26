fe411a03715ef372c20185c23fad114a0195fd24
Grant Regimbal <regimbal.grant@gmail.com>
2016-04-26 16:19:58 -0500

    script to actually install shit

---
+++ go.sh
@@ -0,0 +1,10 @@
+
+#install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+#oh my zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+
+#install spacemacs
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
